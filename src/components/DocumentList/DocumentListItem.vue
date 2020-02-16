9d9bc082f1cd7e91839b73a32f0817f26acb975f
NAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2020-02-17 00:14:54 +0800

    import element-ui && create a too simple list

---
+++ src/components/DocumentList/DocumentListItem.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="item-container">
+        <el-row :gutter="10">
+            <el-col :md="16" :sm="24">
+                <div class="document-title">
+                    {{title}}
+                </div>
+                <div class="document-abstract">
+                    {{abstract}}
+                </div>
+            </el-col>
+            <el-col :md="6" :xs="16">
+                <ul>
+                    <li class="document-author" v-for="(author, index) in authors" :key="index">
+                        {{author.name}}
+                    </li>
+                </ul>
+            </el-col>
+            <el-col class="document-year" :md="2" :xs="8">
+                {{year}}
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DocumentListItem",
+        props: {
+            title: {
+                type: String,
+                default: "Document"
+            },
+            id: {
+                type: Number,
+                default: -1
+            },
+            authors: {
+                type: Array,
+                default: () => {
+                    return []
+                }
+            },
+            abstract: {
+                type: String,
+                default: ""
+            },
+            year: {
+                type: String,
+                default: ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .item-container {
+        border-style: solid;
+        border-width: 1px 0;
+        border-color: rgba(105, 105, 105, 0.32);
+        padding: 20px 10px;
+    }
+
+    .document-title {
+        text-align: left;
+        font-size: 36px;
+        margin: 5px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    .document-abstract {
+        text-align: left;
+        font-size: 14px;
+        margin: 0 0 0 10px;
+        color: dimgrey;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    .document-year {
+        margin: 20px 0;
+    }
+
+    .document-author {
+        font-size: 14px;
+    }
+</style>
---
+++ src/components/DocumentList/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="document-list-container">
+        <div class="document-list-head-search">
+            <el-select class="document-list-search" v-model="searchType">
+                <el-option v-for="type in searchTypes" :key="type.value" :value="type.value" :label="type.label"/>
+            </el-select>
+            <el-input class="document-list-search search-input" v-model="searchKeyword" placeholder="请输入搜索内容"></el-input>
+            <el-button class="document-list-search" type="primary">搜索</el-button>
+        </div>
+        <div class="list-container">
+            <DocumentListItem v-for="document in documents" :key="document.id" :title="document.title" :id="document.id"
+                              :authors="document.authors" :abstract="document.abstract" :year="document.year"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import DocumentListItem from "@/components/DocumentList/DocumentListItem";
+    export default {
+        name: "DocumentList",
+        components: {DocumentListItem},
+        data: function () {
+            return {
+                searchKeyword: '',
+                searchType: 'title',
+                searchTypes: [
+                    { value: 'title', label: '标题' },
+                    { value: 'author', label: '作者' },
+                    { value: 'affiliation', label: '第一作者单位' },
+                    { value: 'publicationTitle', label: '出版物' },
+                    { value: 'abstract', label: '摘要'},
+                    { value: 'publicationYear', label: '出版年份' }
+                ],
+                documents: [
+                    {
+                        title: "Big problems in industry (panel)",
+                        id: "123456",
+                        authors: [{
+                            name: "John Penix",
+                            affiliation: "Google, USA",
+                            id: "123456789"
+                        }],
+                        abstract: "Software Engineering in practice deals with scale in a variety of dimensions. We build large scale systems operating on vast amount of data. We have millions of customers with billions of queries and transactions. We have distributed teams making thousands of changes, running millions of tests and releasing multiple times per day. These dimensions of scale interact to provide challenges for software development tools and processes. The panelists will describe the challenging aspects of scale in their specific problem domains and discuss which software engineering methods work and which leave room for improvement.",
+                        keywords: "SoftwareConferencesSoftware engineeringSecurityCommitteesIndustriesCollaboration",
+                        publication: "2013 28th IEEE/ACM International Conference on Automated Software Engineering (ASE)",
+                        year: "2013",
+                        startPage: "xxiii",
+                        endPage: "xxv",
+                        doi: "10.1109/ASE.2013.6693060",
+                        pdf: "https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=6693060",
+                        authorKeyWord: "Program Repair;JVM Bytecode;Mutation Testing",
+                        ieeeTerms: "Generators;Software;Testing;Indexes;Data mining;Computer science",
+                        inspecControlledTerms: "formal specification;software fault tolerance;temporal logic",
+                        inspecNonControlledTerms: "automated digital controller synthesis tool;physical plants;linear timeinvariant systems;intricate physical plant models;digital control literature;real-world control engineering problems;automated MATLAB",
+                        citationCount: "1",
+                        ref: []
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .document-list-search {
+        display: inline-block;
+        margin: 5px;
+    }
+
+    .document-list-search.search-input {
+        max-width: 400px;
+        min-width: 200px;
+    }
+
+    .document-list-head-search {
+        /*float: right;*/
+    }
+
+    .list-container {
+        margin: 20px 0;
+    }
+</style>
