3f4fd036f497fb6658e75b94a22de70861ac9b3b
lsn <945810689@qq.com>
2020-02-19 01:14:14 +0800

    搭建路由失败

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+/**
+ * 配置vue-router
+ */
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import homeRouter from './Home';
+import searchRouter from './Search';
+Vue.use(VueRouter);
+
+// 路由的数组
+const routes = [
+        homeRouter,
+        searchRouter
+];
+
+const router = new VueRouter({
+    routes
+});
+export default router;
+
+
