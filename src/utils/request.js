cfe833cd863e8af66f48334d6d80ea88e790a089
NAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2020-03-05 13:05:34 +0800

    切换到新的后端

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const service = axios.create({
-    baseURL: 'http://101.37.80.37:8081',
+    // baseURL: 'http://101.37.80.37:8081',
+    baseURL: 'http://47.100.97.17:8082',
     timeout: 5000,
 });
 
