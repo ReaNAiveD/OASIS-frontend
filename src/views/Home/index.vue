8de441c6dc0bae8fd99e4e4a5c54bbc975ea819f
lsn <945810689@qq.com>
2020-03-06 20:40:15 +0800

    no message

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -8,7 +8,7 @@
                     </div>
 
                     <div class="search-body">
-                        <el-input placeholder="请输入作者，出版社，或论文标题" v-model="searchInput" class="input-with-select" @keyup.enter.native="search">
+                        <el-input placeholder="请输入作者，摘要，或论文标题" v-model="searchInput" class="input-with-select" @keyup.enter.native="search">
                             <el-select v-model="select" slot="prepend" placeholder="混合搜索" style="width: 110px" >
                                 <el-option label="混合搜索" value="0"></el-option>
                                 <el-option label="标题" value="1"></el-option>
